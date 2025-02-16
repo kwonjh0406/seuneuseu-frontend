68f0b8a1c50082c35194974c43f7f067bc7706cf
kwonjh0406 <kwonjh0406@gmail.com>
2025-02-16 16:25:21 +0900

    pwa test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next"
 import { Inter } from 'next/font/google'
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,8 +9,17 @@ export const metadata: Metadata = {
   title: "스느스",
   description: "새로운 소통의 시작, 여러분의 생각을 공유하세요",
   manifest: "/manifest.json",
+  viewport:
+    "width=device-width, user-scalable=false, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover",
 }
 
+// export const viewport: Viewport = {
+//   width: "device-width",
+//   initialScale: 1,
+//   maximumScale: 1,
+//   userScalable: false,
+//   viewportFit: "cover",
+// }
 
 export default function RootLayout({
   children,
@@ -20,12 +28,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="min-h-screen bg-background">
