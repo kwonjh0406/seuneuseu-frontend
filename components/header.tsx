c84e16f72e1f0668f8e638b7af4c36ff859a7e90
kwonjh0406 <kwonjh0406@gmail.com>
2025-06-19 12:27:03 +0900

    fix: getUser.tsx api 예외처리

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export const Header = ({ title, loggedInUsername }: HeaderProps) => {
 
   return (
     <header className="max-sm:fixed sm:sticky w-full top-0 z-40 flex items-center justify-between px-4 h-14 border-b bg-background/90 backdrop-blur-md">
-      <h1 className="text-xl font-semibold">{title}</h1>
+      <h1 className="text-xl font-semibold">{title}{user?.username}</h1>
       <div className="flex items-center space-x-2">
         <ThemeToggle />
         {user?.username ? (
